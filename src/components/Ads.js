e6194d988f9ec88383b3741c1657cc928e92cc47
Rachael Arnold <rae.arnold@gmail.com>
2021-04-19 15:10:57 -0400

    Update associate id

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -8,32 +8,32 @@ const Ads = () => {
             <div className="amazon-links">
                 <div
                     dangerouslySetInnerHTML={{
-                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=racharnoonl01-20&language=en_US&marketplace=amazon&region=US&placement=B07NJQYCXM&asins=B07NJQYCXM&linkId=c5121bba1f936258c37070b67be7bbb4&show_border=false&link_opens_in_new_window=true"></iframe>'
+                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=milk-monster-20&language=en_US&marketplace=amazon&region=US&placement=B07NJQYCXM&asins=B07NJQYCXM&linkId=c5121bba1f936258c37070b67be7bbb4&show_border=false&link_opens_in_new_window=true"></iframe>'
                     }}>
                 </div>
                 <div
                     dangerouslySetInnerHTML={{
-                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=racharnoonl01-20&language=en_US&marketplace=amazon&region=US&placement=B0876T9DQZ&asins=B0876T9DQZ&linkId=06dfdfbaed7174af0e4a2e7dcfbb9dfd&show_border=false&link_opens_in_new_window=true"></iframe>'
+                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=milk-monster-20&language=en_US&marketplace=amazon&region=US&placement=B0876T9DQZ&asins=B0876T9DQZ&linkId=06dfdfbaed7174af0e4a2e7dcfbb9dfd&show_border=false&link_opens_in_new_window=true"></iframe>'
                     }}>
                 </div>
                 <div
                     dangerouslySetInnerHTML={{
-                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=racharnoonl01-20&language=en_US&marketplace=amazon&region=US&placement=B07CJHG87R&asins=B07CJHG87R&linkId=bfb7ebb151a22cdfdbcf36df17a15c6e&show_border=false&link_opens_in_new_window=true"></iframe>'
+                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=milk-monster-20&language=en_US&marketplace=amazon&region=US&placement=B07CJHG87R&asins=B07CJHG87R&linkId=bfb7ebb151a22cdfdbcf36df17a15c6e&show_border=false&link_opens_in_new_window=true"></iframe>'
                     }}>
                 </div>
                 <div
                     dangerouslySetInnerHTML={{
-                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=racharnoonl01-20&language=en_US&marketplace=amazon&region=US&placement=B005M03AVO&asins=B005M03AVO&linkId=4211481e0cf76b3ac6e8f7265cac3b84&show_border=false&link_opens_in_new_window=true"></iframe>'
+                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=milk-monster-20&language=en_US&marketplace=amazon&region=US&placement=B005M03AVO&asins=B005M03AVO&linkId=4211481e0cf76b3ac6e8f7265cac3b84&show_border=false&link_opens_in_new_window=true"></iframe>'
                     }}>
                 </div>
                 <div
                     dangerouslySetInnerHTML={{
-                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=racharnoonl01-20&language=en_US&marketplace=amazon&region=US&placement=B07MP9FQ8R&asins=B07MP9FQ8R&linkId=8d59a9fc32ce4413454f474afe45f5b9&show_border=false&link_opens_in_new_window=true"></iframe>'
+                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=milk-monster-20&language=en_US&marketplace=amazon&region=US&placement=B07MP9FQ8R&asins=B07MP9FQ8R&linkId=8d59a9fc32ce4413454f474afe45f5b9&show_border=false&link_opens_in_new_window=true"></iframe>'
                     }}>
                 </div>            
                 <div
                     dangerouslySetInnerHTML={{
-                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=racharnoonl01-20&language=en_US&marketplace=amazon&region=US&placement=B006XISCNA&asins=B006XISCNA&linkId=2359b40154a49d6df7eee7690ecec216&show_border=false&link_opens_in_new_window=true"></iframe>'
+                        __html: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=milk-monster-20&language=en_US&marketplace=amazon&region=US&placement=B006XISCNA&asins=B006XISCNA&linkId=2359b40154a49d6df7eee7690ecec216&show_border=false&link_opens_in_new_window=true"></iframe>'
                     }}>
                 </div>              </div>
             <p><small>* We may receive a small commission if you click on the above affiliate links. Thanks for your help keeping this calculator functional.</small></p>
